2aab29bc7370f26cec20b67e114779fc9c75071b
Shubham Singh <shubhams12333@gmail.com>
2025-04-09 20:30:33 +0530

    handle custom delimiter

--- calculator.py
+++ calculator.py
@@ -1,6 +1,12 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-    numbers = numbers.replace("\n", ",")
-    parts = numbers.split(",")
+
+    delimiter = ","
+    if numbers.startswith("//"):
+        delimiter_line, numbers = numbers.split("\n", 1)
+        delimiter = delimiter_line[2:]
+
+    numbers = numbers.replace("\n", delimiter)
+    parts = numbers.split(delimiter)
     return sum(int(p) for p in parts)
--- test_calculator.py
+++ test_calculator.py
@@ -15,8 +15,12 @@ def test_add_multiple_comma_separated_numbers():
 def test_add_numbers_with_newline_delimiters():
     assert add("11\n12,13") == 36
 
+def test_add_with_custom_delimiter():
+    assert add("//;\n1;2") == 3
+
 test_add_empty_string_returns_0()
 test_add_single_number_returns_itself()
 test_add_two_numbers_comma_separated()
 test_add_multiple_comma_separated_numbers()
 test_add_numbers_with_newline_delimiters()
+test_add_with_custom_delimiter()
