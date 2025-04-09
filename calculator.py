14eb76910a9ba6f5bad9c194a02b21622a4712b4
Shubham Singh <shubhams12333@gmail.com>
2025-04-09 20:20:08 +0530

    handle two numbers

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
-    return int(numbers)
+    parts = numbers.split(",")
+    return sum(int(p) for p in parts)
--- test_calculator.py
+++ test_calculator.py
@@ -6,5 +6,9 @@ def test_add_empty_string_returns_0():
 def test_add_single_number_returns_itself():
     assert add("1") == 1
 
+def test_add_two_numbers_comma_separated():
+    assert add("4,5") == 9
+
 test_add_empty_string_returns_0()
 test_add_single_number_returns_itself()
+test_add_two_numbers_comma_separated()
