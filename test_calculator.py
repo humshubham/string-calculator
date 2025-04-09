d21c1be2c1bd40cc749f172180e7d2b96d32c417
Shubham Singh <shubhams12333@gmail.com>
2025-04-09 20:21:24 +0530

    test multiple numbers

--- test_calculator.py
+++ test_calculator.py
@@ -9,6 +9,10 @@ def test_add_single_number_returns_itself():
 def test_add_two_numbers_comma_separated():
     assert add("4,5") == 9
 
+def test_add_multiple_comma_separated_numbers():
+    assert add("7,8,9,10") == 34
+
 test_add_empty_string_returns_0()
 test_add_single_number_returns_itself()
 test_add_two_numbers_comma_separated()
+test_add_multiple_comma_separated_numbers()
