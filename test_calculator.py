fb4a2fa40414f1a227b1918c7fa38824dfaa936c
Shubham Singh <shubhams12333@gmail.com>
2025-04-09 20:17:03 +0530

    single number input

--- test_calculator.py
+++ test_calculator.py
@@ -3,4 +3,8 @@ from calculator import add
 def test_add_empty_string_returns_0():
     assert add("") == 0
 
+def test_add_single_number_returns_itself():
+    assert add("1") == 1
+
 test_add_empty_string_returns_0()
+test_add_single_number_returns_itself()
